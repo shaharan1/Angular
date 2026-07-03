b323009e061d76c4b58b8816225a782c6ca593d4
Md. Shaharan Hossain <shaharanhossain1@gmail.com>
2026-07-03 22:13:07 +0600

    Add

--- Angular_Modern_Hospital_Management_System/src/app/components/shared/layout/header/header.ts
+++ Angular_Modern_Hospital_Management_System/src/app/components/shared/layout/header/header.ts
@@ -1,13 +1,36 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-header',
-  imports: [],
-  templateUrl: './header.html',
-  styleUrl: './header.css',
+  standalone: true,
+  imports: [CommonModule, RouterModule, FormsModule],
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']
 })
-export class Header {
+export class HeaderComponent {
 
-  constructor() { }
+  pageTitle = 'Dashboard';
+  currentDate = new Date();
 
+  notificationsCount = 5;
+  messagesCount = 2;
+
+  currentUser = {
+    name: 'Dr. Sarah Reynolds',
+    role: 'Chief Attending Physician',
+    avatarInitials: 'SR'
+  };
+
+  isProfileMenuOpen = false;
+
+  toggleProfileMenu(): void {
+    this.isProfileMenuOpen = !this.isProfileMenuOpen;
+  }
+
+  closeProfileMenu(): void {
+    this.isProfileMenuOpen = false;
+  }
 }
