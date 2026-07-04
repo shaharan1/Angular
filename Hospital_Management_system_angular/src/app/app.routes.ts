e63ad29994302a4801827f35afa102fa9dc12d98
Md. Shaharan Hossain <shaharanhossain1@gmail.com>
2026-07-04 19:14:57 +0600

    Add

--- Hospital_Management_system_angular/src/app/app.routes.ts
+++ Hospital_Management_system_angular/src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { DoctorDepartment } from './components/doctor-department/doctor-department';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+ {
+    path: 'doctor-departments',
+    component: DoctorDepartment
+  }
+
+];
---
+++ Hospital_Management_system_angular/src/environments/environment.ts
@@ -0,0 +1,6 @@
+
+export const environment = {
+    production: false,
+    apiUrl: 'http://localhost:8085/api/',
+    imgUrl: 'http://localhost:8085/uploads/'
+};
