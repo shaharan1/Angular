6105a5abe134434efb25632fe8055701f87ef883
Md. Shaharan Hossain <shaharanhossain1@gmail.com>
2026-07-04 17:14:48 +0600

    Add

---
+++ Modern_Hospital_Management_System_Angular/src/app/models/doctorDepartment.model.ts
@@ -0,0 +1,12 @@
+import { DoctorModel } from "./doctor.model";
+
+
+
+export interface DoctorDepartmentModel {
+
+
+    id?: number; // Optional if creating a new department before DB assignment
+    departmentName: string;
+    description?: string;  
+
+}
