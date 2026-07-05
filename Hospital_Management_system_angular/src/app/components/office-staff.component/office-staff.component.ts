9f4e862a0eb3c38c5f52bb6ec8c8d10a378da35b
Md. Shaharan Hossain <shaharanhossain1@gmail.com>
2026-07-05 16:54:33 +0600

    Add

---
+++ Hospital_Management_system_angular/src/app/components/office-staff.component/office-staff.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-office-staff.component',
+  imports: [],
+  templateUrl: './office-staff.component.html',
+  styleUrl: './office-staff.component.css',
+})
+export class OfficeStaffComponent {}
