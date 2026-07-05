b89c6f91f08afd5d00f1964b978d5d7dde688a5e
Md. Shaharan Hossain <shaharanhossain1@gmail.com>
2026-07-05 15:16:48 +0600

    Add

--- Hospital_Management_system_angular/src/app/components/feature/nurse/nurse.ts
+++ Hospital_Management_system_angular/src/app/components/feature/nurse/nurse.ts
@@ -1,9 +1,103 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { NurseModel } from '../../../models/nurseModel';
+import { NurseService } from '../../../services/nurse.service';
 
 @Component({
   selector: 'app-nurse',
-  imports: [],
+  imports: [CommonModule,FormsModule],
   templateUrl: './nurse.html',
   styleUrl: './nurse.css',
 })
-export class Nurse {}
+export class Nurse implements OnInit {
+
+  nurses: NurseModel[] = [];
+
+  nurse: NurseModel = {
+    name: '',
+    email: '',
+    phone: '',
+    password: '',
+    address: '',
+    gender: '',
+    joinDate: '',
+    photo: '',
+    nurseType: '',
+    qualification: '',
+    registrationNumber: '',
+    experienceYears: 0,
+    shift: '',
+    workingHours: '',
+    onDuty: true,
+    assignedWard: '',
+    remarks: ''
+  };
+
+  constructor(private nurseService: NurseService) {}
+
+  ngOnInit(): void {
+    this.loadNurses();
+  }
+
+  loadNurses() {
+    this.nurseService.getAllNurses().subscribe(res => {
+      this.nurses = res;
+    });
+  }
+
+  saveNurse() {
+    this.nurseService.createNurse(this.nurse).subscribe(() => {
+      alert("Nurse Saved Successfully");
+      this.loadNurses();
+      this.resetForm();
+    });
+  }
+
+  dutyStatus(nurse: NurseModel) {
+
+    this.nurseService.updateDutyStatus(
+      nurse.id!,
+      !nurse.onDuty
+    ).subscribe(() => {
+      this.loadNurses();
+    });
+
+  }
+
+  activeStatus(nurse: NurseModel) {
+
+    this.nurseService.updateActiveStatus(
+      nurse.id!,
+      !nurse.active
+    ).subscribe(() => {
+      this.loadNurses();
+    });
+
+  }
+
+  resetForm() {
+
+    this.nurse = {
+      name: '',
+      email: '',
+      phone: '',
+      password: '',
+      address: '',
+      gender: '',
+      joinDate: '',
+      photo: '',
+      nurseType: '',
+      qualification: '',
+      registrationNumber: '',
+      experienceYears: 0,
+      shift: '',
+      workingHours: '',
+      onDuty: true,
+      assignedWard: '',
+      remarks: ''
+    };
+
+  }
+
+}
