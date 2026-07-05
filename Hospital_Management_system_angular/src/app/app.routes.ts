a712534755e0cff580ffa00d37d56788d863c459
Md. Shaharan Hossain <shaharanhossain1@gmail.com>
2026-07-05 15:37:49 +0600

    Add

--- Hospital_Management_system_angular/src/app/app.routes.ts
+++ Hospital_Management_system_angular/src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { Routes } from '@angular/router';
 import { Doctor } from './components/feature/doctor/doctor';
 import { DoctorList } from './components/feature/doctor-list/doctor-list';
 import { DoctorDepartment } from './components/feature/doctor-department/doctor-department';
+import { Nurse } from './components/feature/nurse/nurse';
 
 export const routes: Routes = [
 
@@ -21,6 +22,12 @@ export const routes: Routes = [
   {
     path: 'doctor',
     component: DoctorList
+  },
+
+   {
+    path: 'nurses',
+    component:Nurse
   }
+
 
 ];
--- Hospital_Management_system_angular/src/app/components/feature/nurse-list/nurse-list.ts
+++ Hospital_Management_system_angular/src/app/components/feature/nurse-list/nurse-list.ts
@@ -1,9 +1,103 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { NurseModel } from '../../../models/nurseModel';
+import { NurseService } from '../../../services/nurse.service';
 
 @Component({
   selector: 'app-nurse-list',
-  imports: [],
+  imports: [CommonModule,FormsModule],
   templateUrl: './nurse-list.html',
   styleUrl: './nurse-list.css',
 })
-export class NurseList {}
+export class NurseList implements OnInit {
+
+
+nurses: NurseModel[] = [];
+  filteredNurses: NurseModel[] = [];
+
+  searchText = '';
+  ward = '';
+
+  constructor(private nurseService: NurseService) {}
+
+  ngOnInit(): void {
+    this.loadNurses();
+  }
+
+  loadNurses(): void {
+    this.nurseService.getAllNurses().subscribe({
+      next: (data) => {
+        this.nurses = data;
+        this.filteredNurses = data;
+      },
+      error: (err) => console.error(err)
+    });
+  }
+
+  search(): void {
+
+    const keyword = this.searchText.toLowerCase();
+
+    this.filteredNurses = this.nurses.filter(n =>
+      n.name.toLowerCase().includes(keyword) ||
+      n.email.toLowerCase().includes(keyword) ||
+      n.phone.toLowerCase().includes(keyword)
+    );
+
+  }
+
+  searchWard(): void {
+
+    if (!this.ward) {
+      this.loadNurses();
+      return;
+    }
+
+    this.nurseService.getNurseByWard(this.ward).subscribe({
+      next: (data) => {
+        this.filteredNurses = data;
+      }
+    });
+
+  }
+
+  loadOnDuty(): void {
+
+    this.nurseService.getOnDutyNurses().subscribe({
+      next: (data) => {
+        this.filteredNurses = data;
+      }
+    });
+
+  }
+
+  reset(): void {
+
+    this.searchText = '';
+    this.ward = '';
+    this.loadNurses();
+
+  }
+
+  changeDutyStatus(nurse: NurseModel): void {
+
+    this.nurseService
+      .updateDutyStatus(nurse.id!, !nurse.onDuty)
+      .subscribe(() => {
+        this.loadNurses();
+      });
+
+  }
+
+  changeActiveStatus(nurse: NurseModel): void {
+
+    this.nurseService
+      .updateActiveStatus(nurse.id!, !nurse.active)
+      .subscribe(() => {
+        this.loadNurses();
+      });
+
+  }
+  
+}
