ca394ecc3fc9701162d81e8d5c657643235a55fe
Md. Shaharan Hossain <shaharanhossain1@gmail.com>
2026-07-11 15:00:10 +0600

    add

---
+++ Hospital_Management_system_angular/src/app/models/AppointmentResponseModel.ts
@@ -0,0 +1,41 @@
+export interface AppointmentResponseModel {
+
+  id: number;
+
+  appointmentNumber: string;
+
+  status: string;
+
+  patientName: string;
+
+  mobileNumber: string;
+
+  specialization: string;
+
+  problemDescription: string;
+
+  appointmentDate: string;
+
+  appointmentTime: string;
+
+  feeCharged: number;
+
+  paymentMethod: string;
+
+  transactionId: string;
+
+  registeredPatientId: number;
+
+  doctorId: number;
+
+  doctorName: string;
+
+  doctorChamber: string;
+
+  scheduleSlotId: number;
+
+  slotIsBooked: boolean;
+
+  doctorSpecialization: string;
+
+}
