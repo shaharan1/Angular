ce8a10ffbf14114612bf40e8e63188c2c8ebe8f7
Md. Shaharan Hossain <shaharanhossain1@gmail.com>
2026-07-23 00:34:08 +0600

    Add

---
+++ Hospital_Management_system_angular/src/app/models/ward.model.ts
@@ -0,0 +1,17 @@
+export interface WardModel {
+
+    id: number;
+
+    name: string;
+
+    departmentId: number;
+
+    departmentName: string;
+
+    roomType: string;
+
+    totalBeds: number;
+
+    basePricePerDay: number;
+
+}
