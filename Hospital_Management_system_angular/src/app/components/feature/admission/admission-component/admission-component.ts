3bc513caf4c2d8ccee9fd5edd44fa5d95a7ae42c
Md. Shaharan Hossain <shaharanhossain1@gmail.com>
2026-07-23 00:56:56 +0600

    Add

---
+++ Hospital_Management_system_angular/src/app/components/feature/admission/admission-component/admission-component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-admission-component',
+  imports: [],
+  templateUrl: './admission-component.html',
+  styleUrl: './admission-component.css',
+})
+export class AdmissionComponent {}
